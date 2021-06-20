6d96cc6b14ab0c54dc31e2b63791678d42c7d81f
Spawar93 <86187504+Spawar93@users.noreply.github.com>
2021-06-20 16:31:28 +0530

    Update buyshare.java
    buyshare class edited

--- Velocity/src/main/java/zerodha/classes/buyshare.java
+++ Velocity/src/main/java/zerodha/classes/buyshare.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class buyshare {
 
 	@FindBy (xpath="//input[@id='search-input']") private WebElement searchtab; //searchbutton
-	
+	System.out.println("buyshare class edited");
 	
 
 }
